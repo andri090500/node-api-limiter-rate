47e28a4517241a5603c2dcc99969734a02c91557
andri090500 <firmansyahandri.9500@gmail.com>
2023-02-21 09:25:17 +0700

    code node for limiter rate api

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const data = require("./data.js");
+const rateLimit = require("express-rate-limit");
+
+const app = express();
+const limiter = rateLimit({
+  // windowMs: 15 * 60 * 1000, // 15 minutes
+  // max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  // standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  // legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  max: 5,
+  windowsMs: 10000, // 10 detik
+});
+
+app.get("/data", limiter, (req, res) => {
+  res.send({
+    status: "success",
+    data: data,
+  });
+});
+
+app.listen(3004, () => console.log("Server Running at port 3004"));
---
+++ data.js
@@ -0,0 +1,14 @@
+const data = [
+  {
+    id: 1,
+    nama: "Andri Firmansyah",
+  },
+  { id: 2, nama: "Naruto" },
+  {
+    id: 3,
+    nama: "Sasuke",
+  },
+  { id: 4, nama: "Obito" },
+];
+
+module.exports = data;
